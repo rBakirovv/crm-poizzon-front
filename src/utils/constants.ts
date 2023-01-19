4d97b311d38aef4559c07d025a6db1a38e9a05ac
Ruslan <saharcorez@gmail.com>
2023-01-19 14:31:10 +0300

    small fixes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,8 @@
-export const BASE_URL = "http://localhost:3001";
+//export const BASE_URL = "http://localhost:3001";
 //export const BASE_URL = "http://192.168.0.102:3001";
-export const BASE_URL_FRONT = "http://localhost:3000";
+//export const BASE_URL_FRONT = "http://localhost:3000";
+export const BASE_URL = "http://185.228.234.74";
+export const BASE_URL_FRONT = "http://185.228.234.74";
 export const SUPERADMIN = "Создатель";
 export const ADMIN = "Администратор";
 export const BUYER = "Байер";
