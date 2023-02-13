d025c0ddc4fc1a7e3863b51958836fe5cbbb3fc8
Ruslan <saharcorez@gmail.com>
2023-02-13 21:02:18 +0300

    fix: load

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -84,6 +84,9 @@ const Home = observer(() => {
           />
           <Navigation />
           <Main>
+            {
+              (OrderData.orders.length === 0 || !OrderData.orders.length) && typeof window === "undefined" && <Preloader />
+            }
             {OrderData.orders.length > 0 && typeof window !== "undefined" && (
               <OrdersList />
             )}
