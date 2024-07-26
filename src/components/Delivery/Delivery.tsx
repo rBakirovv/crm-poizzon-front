c0046e9a6ab105aff21df6bd6646a91ec9887237
Ruslan <saharcorez@gmail.com>
2024-07-26 17:59:52 +0300

    feat: delivery address for warehouse

--- src/components/Delivery/Delivery.tsx
+++ src/components/Delivery/Delivery.tsx
@@ -1596,6 +1596,28 @@ const Delivery = observer(() => {
           />
           <label>Экспресс</label>
         </div>
+        {UserData.userData.position === "Работник склада" &&
+          OrderData.order.deliveryAddress !== "" && (
+            <div style={{marginBottom: "1rem"}}>
+              <h4>Адрес доставки</h4>
+              <div className={styles["delivery__input-container"]}>
+                <p className={styles["delivery-copy"]} onClick={copyAddress}>
+                  {OrderData.order.deliveryAddress}{" "}
+                  <svg
+                    x="0px"
+                    y="0px"
+                    width="24px"
+                    height="24px"
+                    viewBox="0 0 24 24"
+                    focusable="false"
+                    fill="currentColor"
+                  >
+                    <path d="M3.9,12c0-1.7,1.4-3.1,3.1-3.1h4V7H7c-2.8,0-5,2.2-5,5s2.2,5,5,5h4v-1.9H7C5.3,15.1,3.9,13.7,3.9,12z M8,13h8v-2H8V13zM17,7h-4v1.9h4c1.7,0,3.1,1.4,3.1,3.1s-1.4,3.1-3.1,3.1h-4V17h4c2.8,0,5-2.2,5-5S19.8,7,17,7z"></path>
+                  </svg>
+                </p>
+              </div>
+            </div>
+          )}
         <div className={styles["delivery-packages__container"]}>
           {OrderData.order.deliveryAddress !== "" &&
             OrderData.order.deliveryEntity !== "" && (
--- src/components/OrderChange/OrderChange.tsx
+++ src/components/OrderChange/OrderChange.tsx
@@ -1430,6 +1430,39 @@ const OrderChange: FC<IOrderChangeProps> = observer(({ payments }) => {
               )}
             </div>
           )}
+          {UserData.userData.position === "Работник склада" && (
+            <div className={styles["order-change__public-link-container"]}>
+              <p className={styles["order-change__public-link-text"]}>
+                Публичная ссылка
+              </p>
+              <a
+                className={styles["order-change__public-link"]}
+                href={`${BASE_URL_FRONT}/order/${OrderData.order._id}/${OrderData.order.orderId}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {BASE_URL_FRONT}/order/{OrderData.order._id}/
+                {OrderData.order.orderId}
+              </a>
+              <div
+                onClick={copyLink}
+                className={styles["order-change__public-link-text-copy"]}
+              >
+                {!isCopy ? "Скопировать" : "Cкопировано в буфер обмена"}{" "}
+                <svg
+                  x="0px"
+                  y="0px"
+                  width="24px"
+                  height="24px"
+                  viewBox="0 0 24 24"
+                  focusable="false"
+                  fill="currentColor"
+                >
+                  <path d="M3.9,12c0-1.7,1.4-3.1,3.1-3.1h4V7H7c-2.8,0-5,2.2-5,5s2.2,5,5,5h4v-1.9H7C5.3,15.1,3.9,13.7,3.9,12z M8,13h8v-2H8V13zM17,7h-4v1.9h4c1.7,0,3.1,1.4,3.1,3.1s-1.4,3.1-3.1,3.1h-4V17h4c2.8,0,5-2.2,5-5S19.8,7,17,7z"></path>
+                </svg>
+              </div>
+            </div>
+          )}
           <form
             onSubmit={openSubmitPopup}
             className={styles["order-change__order-form"]}
