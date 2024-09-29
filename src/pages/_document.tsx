8f38fcf825de69697eb9d1a880dd033f444415e3
Ruslan <saharcorez@gmail.com>
2024-09-29 22:38:32 +0300

    delete widjet

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,11 +12,6 @@ export default function Document() {
         <div id="burger"></div>
         <div id="submit-popup"></div>
         <div id="login-popup"></div>
-        <div id="widjet-popup"></div>
-        <script
-          id="ISDEKscript"
-          src="https://widget.poizonqq.ru/widget/widjet.js"
-        ></script>
       </body>
     </Html>
   );
